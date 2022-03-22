7f2162a69a8b6cf9fa423192cf1b3c3261713155
mangesh9903 <mangeshb9903@gmail.com>
2022-03-23 01:00:30 +0530

    ADDED: Method to add element in Linked List.

--- src/test/java/com/bridgelabz/linkedlisttest/MyNodeTest.java
+++ src/test/java/com/bridgelabz/linkedlisttest/MyNodeTest.java
@@ -1,15 +1,17 @@
 package com.bridgelabz.linkedlisttest;
 
-import com.bridgelabz.linkedlist.MyNode;
+import com.bridgelabz.linkedlist.INode;
+import com.bridgelabz.linkedlist.NewLinkedList;
+import com.bridgelabz.linkedlist.Node;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MyNodeTest {
     @Test
     public void givenNumber_whenLinked_shouldReturnTrue() {
-        MyNode<Integer> firstNode = new MyNode<>(56);
-        MyNode<Integer> secondNode = new MyNode<>(30);
-        MyNode<Integer> thirdNode = new MyNode<>(70);
+        INode<Integer> firstNode = new Node<>(56);
+        INode<Integer> secondNode = new Node<>(30);
+        INode<Integer> thirdNode = new Node<>(70);
 
         firstNode.setNext(secondNode);
         secondNode.setNext(thirdNode);
@@ -17,6 +19,24 @@ public class MyNodeTest {
         boolean result = firstNode.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode);
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void given3Numbers_whenAdded_toLinkedListShouldAdd() {
+        NewLinkedList linkedList = new NewLinkedList();
+        INode<Integer> firstNode = new Node<>(70);
+        INode<Integer> secondNode = new Node<>(30);
+        INode<Integer> thirdNode = new Node<>(56);
+
+        linkedList.add(firstNode);
+        linkedList.add(secondNode);
+        linkedList.add(thirdNode);
+        linkedList.printNodes();
+
+        boolean result = linkedList.head.equals(thirdNode) && linkedList.head.getNext().equals(secondNode) &&
+                linkedList.tail.equals(firstNode);
+        Assert.assertTrue(result);
+    }
+
 }
 
 
