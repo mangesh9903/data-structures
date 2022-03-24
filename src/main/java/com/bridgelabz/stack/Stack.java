bddc5ba78908ac353326a6c85d8209d0c625af36
mangesh9903 <mangeshb9903@gmail.com>
2022-03-25 02:11:12 +0530

    ADDED: Funtionality to Push And Pop Element In Stack.

--- src/main/java/com/bridgelabz/stack/Stack.java
+++ src/main/java/com/bridgelabz/stack/Stack.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.stack;
 
+import com.bridgelabz.linkedlist.INode;
 import com.bridgelabz.linkedlist.NewLinkedList;
 
 public class Stack {
@@ -7,6 +8,18 @@ public class Stack {
 
     public Stack() {
         this.linkedList = new NewLinkedList();
+    }
+
+    public void push(INode node) {
+        linkedList.add(node);
+    }
+
+    public INode pop() {
+        return linkedList.pop();
+    }
+
+    public void printStack() {
+        linkedList.printNodes();
     }
 
 }
