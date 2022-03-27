9cbfe24d097857b123c151128183e3deb4953c79
mangesh9903 <mangeshb9903@gmail.com>
2022-03-28 00:13:44 +0530

    Create Binary Search Tree.

---
+++ src/main/java/com/bridgelabz/binarysearchtree/controller/HomeControllerBST.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.binarysearchtree.controller;
+
+public class HomeControllerBST {
+    public static void main(String[] args) {
+        System.out.println(" Welcome to Binary Search Tree.");
+    }
+}
