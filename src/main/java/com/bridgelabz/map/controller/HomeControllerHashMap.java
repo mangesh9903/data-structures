c28af022086da176171ec636103a0fc1f200eb2c
mangesh9903 <mangeshb9903@gmail.com>
2022-03-28 07:52:12 +0530

    Welcome To HashMap.

---
+++ src/main/java/com/bridgelabz/map/controller/HomeControllerHashMap.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.map.controller;
+
+public class HomeControllerHashMap {
+    public static void main(String[] args) {
+        System.out.println("Welcome To HashMap.");
+    }
+}
